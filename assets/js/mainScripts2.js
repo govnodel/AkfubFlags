c15ab1c82741d95763dd25167fbf1bd7a4576d68
Resonare <ioepochoi@gmail.com>
2019-06-12 13:47:33 +1100

    huh

--- assets/js/mainScripts2.js
+++ assets/js/mainScripts2.js
@@ -4,21 +4,18 @@ var into = "into 1s linear forwards";
 var outto = "outto 1s linear forwards";
 var first = document.getElementById("_1");
 var second = document.getElementById("_2");
+var WIDTH = document.documentElement.clientHeight / 2;
 
 window.onscroll = function(){
-	scrolled = window.pageYOffset || document.documentElement.scrollTop;
-	if (scrolled > 400) {
+	scrolled = window.pageYOffset;
+	if (scrolled > WIDTH) {
     document.documentElement.clientWidth
     first.style.animation = into;
-    // first.style.animationFillMode = "forwards";
     second.style.animation = into;
-    // second.style.animationFillMode = "forwards";
     was = true;
 	} else if(was){
     first.style.animation = outto;
-    // first.style.animationFillMode = "forwards";
     second.style.animation = outto;
-    // second.style.animationFillMode = "forwards";
     was = false;
 	}
 }
