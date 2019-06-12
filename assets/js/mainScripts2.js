12c14139701f32c36978afbab43dc911e32eac48
Resonare <ioepochoi@gmail.com>
2019-06-12 18:12:30 +1100

    ughyg

--- assets/js/mainScripts2.js
+++ assets/js/mainScripts2.js
@@ -1,9 +1,11 @@
 var scrolled;
 var was = false;
+var up = true;
+var can = true;
 var into = "into 1s linear forwards";
 var outto = "outto 1s linear forwards";
-var first = document.getElementById("_1");
-var second = document.getElementById("_2");
+var first = document.getElementById("first");
+var second = document.getElementById("second");
 var WIDTH = document.documentElement.clientHeight / 2;
 
 window.onscroll = function(){
@@ -18,4 +20,30 @@ window.onscroll = function(){
     second.style.animation = outto;
     was = false;
 	}
+
+	if ((scrolled > 100)&&(up)&&(can)){
+		up = false;
+		can = false;
+		setTimeout(invert, 450);
+		$('html, body').animate({
+			scrollTop: $('#second').offset().top
+		}, 500);
+	} else if ((scrolled < WIDTH * 2 - 100)&&(!up)&&(can)){
+		up = true;
+		can = false;
+		setTimeout(invert, 450);
+		$('html, body').animate({
+			scrollTop: $('#first').offset().top
+		}, 500);
+	}
+}
+
+function invert() {
+  can = true;
+}
+
+function slowScroll(){
+	$('html, body').animate({
+		scrollTop: $('#second').offset().top
+	}, 500);
 }
