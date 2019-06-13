6bfd56f229d5a55db23a2f39f77f7e7ab29fecdd
Resonare <ioepochoi@gmail.com>
2019-06-13 23:19:10 +1100

    ss

--- assets/js/mainScripts2.js
+++ assets/js/mainScripts2.js
@@ -2,16 +2,41 @@ var scrolled;
 var was = false;
 var up = true;
 var can = true;
+var pushed = true;
 var into = "into 1s linear forwards";
 var outto = "outto 1s linear forwards";
 var first = document.getElementById("first");
 var second = document.getElementById("second");
-var WIDTH = document.documentElement.clientHeight / 2;
+var HEIGHT = document.documentElement.clientHeight / 2;
+
+$('#buttonprofile').bind("click", pushMenu);
+
+function pushMenu(){
+	if (pushed){
+		$('#profilemenu').animate({
+			left: '0px'
+		}, 200);
+		$('#buttonprofile').delay(500);
+		$('#buttonprofile').animate({
+			left: '25%'
+		}, 500);
+		pushed = false;
+	} else {
+		$('#buttonprofile').animate({
+			left: '0px'
+		}, 500);
+		$('#profilemenu').delay(500);
+		$('#profilemenu').animate({
+			left: '-30%'
+		}, 200);
+		pushed = true;
+	}
+}
 
 window.onscroll = function(){
 	scrolled = window.pageYOffset;
 	// alert(scrolled);
-	if (scrolled > WIDTH) {
+	if (scrolled > HEIGHT) {
     document.documentElement.clientWidth
     first.style.animation = into;
     second.style.animation = into;
@@ -30,7 +55,7 @@ window.onscroll = function(){
 		$('html, body').animate({
 			scrollTop: $('#second').offset().top
 		}, 500);
-	} else if ((scrolled < WIDTH * 2 - 10)&&(!up)&&(can)){
+	} else if ((scrolled < HEIGHT * 2 - 10)&&(!up)&&(can)){
 		// alert("up");
 		up = true;
 		can = false;
