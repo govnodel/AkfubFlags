5e022913c26ead1980455b86b3be01232d749fb3
Resonare <ioepochoi@gmail.com>
2019-06-14 16:59:52 +1100

    111

--- assets/js/mainScripts2.js
+++ assets/js/mainScripts2.js
@@ -5,47 +5,42 @@ var can = true;
 var pushed = true;
 var into = "into 1s linear forwards";
 var outto = "outto 1s linear forwards";
-var first = document.getElementById("first");
-var second = document.getElementById("second");
+var profile = document.getElementById('profilemenu');
+var button = document.getElementById('buttonprofile');
+var first = document.getElementById('first');
+var second = document.getElementById('second');
+var arrow = document.getElementById('proArrow');
 var HEIGHT = document.documentElement.clientHeight / 2;
 
-// $('#buttonprofile').bind("click", pushMenu);
-//
-// function pushMenu(){
-// 	if (pushed){
-// 		$('#profilemenu').animate({
-// 			left: '0px'
-// 		}, {duration: 200, easing: "linear" });
-// 		$('#buttonprofile').delay(200);
-// 		$('#buttonprofile').animate({
-// 			left: '30%'
-// 		}, {duration: 500, easing: "linear" });
-// 		pushed = false;
-// 	} else {
-// 		$('#buttonprofile').animate({
-// 			left: '0px'
-// 		}, {duration: 500, easing: "linear" });
-// 		$('#profilemenu').animate({
-// 			left: '-30%'
-// 		}, {duration: 200, easing: "linear" });
-// 		pushed = true;
-// 	}
-// }
+$('#buttonprofile').bind("click", pushMenu);
+
+function pushMenu(){
+	if (pushed){
+		profile.style.animation = 'push 2s ease forwards';
+    button.style.animation = 'pushButton 2s ease forwards';
+		pushed = false;
+	} else {
+		profile.style.animation = 'pull 2s ease forwards';
+		button.style.animation = 'pullButton 2s ease forwards';
+		pushed = true;
+	}
+}
 
 window.onscroll = function(){
 	scrolled = window.pageYOffset;
 	if (scrolled > HEIGHT) {
     first.style.animation = into;
     second.style.animation = into;
+		arrow.style.animation = into;
     was = true;
 	} else if(was){
     first.style.animation = outto;
     second.style.animation = outto;
+		arrow.style.animation = outto;
     was = false;
 	}
 
 	if ((scrolled > 10)&&(up)&&(can)){
-		// alert("down");
 		up = false;
 		can = false;
 		setTimeout(invert, 510);
@@ -53,7 +48,6 @@ window.onscroll = function(){
 			scrollTop: $('#second').offset().top
 		}, 500);
 	} else if ((scrolled < HEIGHT * 2 - 10)&&(!up)&&(can)){
-		// alert("up");
 		up = true;
 		can = false;
 		setTimeout(invert, 510);
@@ -68,7 +62,6 @@ function invert() {
 }
 
 function slowScroll(){
-	// alert("slowScroll");
 	can = false;
 	setTimeout(invert, 510);
 	$('html, body').animate({
