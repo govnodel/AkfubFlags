d91a215603c5248297ed00d236a08b5485483dd5
Resonare <ioepochoi@gmail.com>
2019-08-10 19:15:18 +1100

    dasdas

--- assets/php/registration.php
+++ assets/php/registration.php
@@ -35,16 +35,14 @@
   if (!$error){
     $query = pg_query($connect, "SELECT * FROM ourusers WHERE name = '".$login."'");
     $numrows = pg_num_rows($query);
-    echo $numrows == 0;
-    // if($numrows == 0){
+    if($numrows == 0){
     //   $result = mysql_query("INSERT INTO ourusers(login, password, mail, stats)
     //   VALUES('$login','$pass', '$email', 'hi')");
     //   header("Location: welcome.php");
-    // } else {
-    //   $error_login = "That username already exists";
-    // }
+    } else {
+      $error_login = "That username already exists";
+    }
     // exit;
-    // header("Location: welcome.php");
   }
  }
  ?>
