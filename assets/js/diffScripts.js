d3ec811179956cf41c4b9613bcc699a9092a7291
Resonare <ioepochoi@gmail.com>
2019-08-20 12:38:01 +1100

    онаопнр

--- assets/js/diffScripts.js
+++ assets/js/diffScripts.js
@@ -1,4 +1,15 @@
-$('#tr1').animate({
-  left: '-100%'
-}, 500);
-$('#tr1, #tr2').fadeOut(0).fadeIn(1000);
+// $( document ).ready(function() {
+//   $('#tr1').animate({
+//     // left: '-100%'
+//     left: "+=50"
+//   }, 500);
+//   // alert("hile");
+// });
+// // $('#tr1, #tr2').fadeOut(0).fadeIn(1000);
+$( "#tr1" ).click(function() {
+  $( "svg" ).animate({
+    opacity: 0.25,
+    left: "-100%",
+    // height: "toggle"
+  }, 1000);
+});
