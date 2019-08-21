f23634557aecd936319b1735104f4dc1af336027
Resonare <ioepochoi@gmail.com>
2019-08-21 19:16:18 +1100

    no

---
+++ assets/js/newGameScripts.js
@@ -0,0 +1,32 @@
+//fill flags first time
+    //1) choose 4 random flags from list
+    //2) set random position
+    //3) set name of country
+    //4) add event listeners
+    //5) set timer
+//set all elements' opacity to 1
+//start timer
+//waiting clicks ========================
+//check answer
+    //1) right or not?
+    //2) final or not?
+        //exit if lose
+//refresh
+//waiting timer ========================
+//decrease lives
+//check final or not?
+    //exit if lose
+//refresh
+
+var names = [];
+var flags = [];
+
+function setFlags(){
+  let numbers = [];
+  for (let i = 0; i < 4; i++){
+    let num = Math.round(Math.random() * (names.length - 1));
+    while (num != ) {
+
+    }
+  }
+}
