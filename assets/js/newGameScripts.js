74cdb3fe069953e1be53fe870488cb828fa12251
Resonare <ioepochoi@gmail.com>
2019-08-21 19:47:17 +1100

    fsfd

--- assets/js/newGameScripts.js
+++ assets/js/newGameScripts.js
@@ -20,13 +20,35 @@
 
 var names = [];
 var flags = [];
+var winPos;
+var winName;
 
 function setFlags(){
   let numbers = [];
-  for (let i = 0; i < 4; i++){
-    let num = Math.round(Math.random() * (names.length - 1));
-    while (num != ) {
-
+  for (let i = 0; i < count; i++){
+    numbers[i] = Math.round(Math.random() * (names.length - 1));
+    for (let j = 0; j < numbers.length; j++) {
+      if(i == j){
+        continue;
+      } else {
+        if(numbers[i] == numbers[j]){
+          i--;
+          break;
+        }
+      }
     }
+  }
+
+  for(let i = 0; i < numbers.length; i++){
+    flags[i] = names[numbers[i]];
+  }
+  winPos = Math.round(Math.random() * 3);
+  winName = flags[winPosition];
+
+  nameEl.textContent = winName;
+  for(var i = 0; i < 4; i++){
+    flagsEl[i] = document.getElementById("_" + (i + 1));
+    flagsEl[i].addEventListener("click",clickFlag);
+    flagsEl[i].src = "assets/images/flags/" + flagsFinal[i].replace(/ /g, "_") + ".png";
   }
 }
