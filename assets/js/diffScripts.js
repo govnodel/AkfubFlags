40eb9a14e2c98314613f70192d499f26f6f5d512
Resonare <karpa4ev.i@yandex.ru>
2019-09-13 18:52:50 +1100

    sad

--- assets/js/diffScripts.js
+++ assets/js/diffScripts.js
@@ -4,12 +4,12 @@ $( document ).ready(function() {
   $(".pol1").animate({
     marginRight: '0',
     marginTop: '0'
-  }, 600);
+  }, 400);
 
   $(".pol2").animate({
     marginLeft: '0',
     marginTop: '0'
-  }, 600, function(){
+  }, 400, function(){
     document.body.style.animation = "white 0.1s linear forwards";
   });
 
@@ -27,15 +27,15 @@ $( document ).ready(function() {
 function redir(){
   $(".upblock").animate({
     height: '100vh'
-  }, 700, function(){
+  }, 500, function(){
     $(".chooseArea").css("opacity", "0");
     setTimeout(function(){
       $(".upblock").animate({
         height: '16vh'
-      }, 600);
+      }, 460);
       setTimeout(function(){
         window.location.href = "gayme.php?op=" + regime;
-      }, 600);
-    }, 2000);
+      }, 460);
+    }, 140);
   });
 }
