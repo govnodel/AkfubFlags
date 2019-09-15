177e1aa3401a04adf1297d154c261618efdf70dc
Resonare <karpa4ev.i@yandex.ru>
2019-09-15 20:36:43 +1100

    sad

--- assets/js/diffScripts.js
+++ assets/js/diffScripts.js
@@ -1,16 +1,36 @@
 var regime;
 
 $( document ).ready(function() {
-  $(".pol1").animate({
-    marginRight: '0',
-    marginTop: '0'
-  }, 600);
 
-  $(".pol2").animate({
-    marginLeft: '0',
-    marginTop: '0'
-  }, 600, function(){
-    document.body.style.animation = "white 0.1s linear forwards";
+  $(".flack").animate({
+    opacity: 1
+  }, 800);
+
+  $(".cover").animate({
+    opacity: 0.7
+  }, 800);
+
+  var currentX = '';
+  var currentY = '';
+  var movementConstant = .035;
+  $(document).mousemove(function(e) {
+
+    if(currentX == '') currentX = e.pageX;
+    var xdiff = e.pageX - currentX;
+    currentX = e.pageX;
+
+    if(currentY == '') currentY = e.pageY;
+    var ydiff = e.pageY - currentY;
+    currentY = e.pageY;
+
+    $('.flack').each(function(i, el) {
+      var movement = (i + 1) * (xdiff * movementConstant);
+	    var movementy = (i + 1) * (ydiff * movementConstant);
+      var newX = $(el).position().left + movement;
+	    var newY = $(el).position().top + movementy;
+      $(el).css('left', newX + 'px');
+	    $(el).css('top', newY + 'px');
+    });
   });
 
   $("#historical").on("click", function() {
