270d925b44017d35d4beb3b9df6a11aaa332c2ce
Resonare <karpa4ev.i@yandex.ru>
2019-09-16 20:59:28 +1100

    sad

--- gayme.php
+++ gayme.php
@@ -17,7 +17,7 @@ require "assets/php/gameSet.php"
     <svg class="kruglyash" width="91" height="91" fill = 'none'>
       <circle id = "circle" cx = "-45" cy = "45" r = "40"/>
       <text id="timer_sec" x = "36" y = "56" fill = "white" >10</text>
-      <circle id = "circle2" cx = "-45" cy = "45" r = "40"/>
+      <circle id = "innerCircle" cx = "-45" cy = "45" r = "40"/>
     </svg>
     <p id = "country">Lubava</p>
     <div id="nolives">
