bf9c04b9caed38f3837bad53eca395d25db00de5
Resonare <karpa4ev.i@yandex.ru>
2019-09-20 13:21:57 +1100

    sad

--- assets/js/diffScripts.js
+++ assets/js/diffScripts.js
@@ -2,7 +2,7 @@ var regime;
 var diff = 1;
 const maxDiff = 2;
 $( document ).ready(function() {
-  document.getElementById("circle").setAttributeNS(null, 'x', 48);
+  document.getElementById("timer_sec").setAttributeNS(null, 'x', 48);
   setTimeout(function(){
     $(".flack, .chooseArea *").animate({
       opacity: 1
