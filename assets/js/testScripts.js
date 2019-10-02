85aed7c677fbcdbb3461738ce94cc09c387d8acf
Resonare <47446377+Resonare@users.noreply.github.com>
2019-10-02 20:03:32 +1100

    sad

--- assets/js/testScripts.js
+++ assets/js/testScripts.js
@@ -4,5 +4,5 @@ function sendStats() {
   // let date = new Date(Date.now() + 86400e3);
   // date = date.toUTCString();
 
-  document.cookie = "testT=John Smith; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+  document.cookie = "testT=John Smith; expires=Thu, 18 Dec 2020 12:00:00 UTC; path=/";
 }
