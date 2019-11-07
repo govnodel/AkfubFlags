f0df3266cef0f4f41022e6220580db57c8da89df
Resonare <47446377+Resonare@users.noreply.github.com>
2019-11-07 16:53:12 +1100

    sad

--- assets/php/insertStats.php
+++ assets/php/insertStats.php
@@ -14,19 +14,20 @@
 
   //if ($_COOKIE["userIdInFlags"] != "") {
     $id = 3;//$_COOKIE["userIdInFlags"]; //example 0:4:3:(score):Spain
-    $query = pg_query($connect, "SELECT victories, games, answersProcent, answersQuantity, score FROM ourusers WHERE id = ".$id.";");
+    $query = pg_query($connect, "SELECT victories FROM ourusers WHERE id = ".$id.";");//, games, answersProcent, answersQuantity, score
     while($row = pg_fetch_row($query)){
-      $victories = $row[0];
-      $games = $row[1];
-      $ansProc = $row[2];
-      $ansQua = $row[3];
-      $score = $row[4];
+      echo $row[0];
+      // $victories = $row[0];
+    //   $games = $row[1];
+    //   $ansProc = $row[2];
+    //   $ansQua = $row[3];
+    //   $score = $row[4];
     }
-    echo $victories;
-    echo $games;
-    echo $ansProc;
-    echo $ansQua;
-    echo $score;
+    // echo $victories;
+    // echo $games;
+    // echo $ansProc;
+    // echo $ansQua;
+    // echo $score;
   //}
 
 //$newStats
