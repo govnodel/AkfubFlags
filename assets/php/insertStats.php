e12291f9b0f48beba5a71c07bd18e0561af3c253
Resonare <47446377+Resonare@users.noreply.github.com>
2019-12-09 19:09:37 +1100

    sad

--- assets/php/insertStats.php
+++ assets/php/insertStats.php
@@ -45,10 +45,11 @@
     }
 
     $result = pg_query($connect, "UPDATE ourusers SET victories = ".$victories.", games = ".$games.", percent = ".$ansProc.",
-        quantity = ".$ansQua.", score = ".$score." WHERE id = ".$_COOKIE["userIdInFlags"].";");
+        quantity = ".$ansQua.", score = ".$score." WHERE id = ".$_COOKIE["userIdInFlags"]);
 
     if (!$result) {
-      echo "errorEnd";
+      echo $_COOKIE["userIdInFlags"];
+      echo " errorEnd";
       exit;
     }
   }
