e55c8e7bb1cfe17ba3cde84563ff660a707ddde8
Resonare <47446377+Resonare@users.noreply.github.com>
2019-12-11 21:16:29 +1100

    sad

--- assets/php/insertStats.php
+++ assets/php/insertStats.php
@@ -1,4 +1,5 @@
 <?php
+  echo $_COOKIE["statsInFlags"];
   if ($_COOKIE["userIdInFlags"] != "") {
     //example 0:4:3:(score):Spain:France:Geramny
     $query = pg_query($connect, "SELECT victories, games, percent, quantity, score FROM ourusers WHERE id = ".$_COOKIE["userIdInFlags"].";");
