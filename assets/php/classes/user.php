6724a37e8d93fc6954ddc475833b7bda6e1be438
Resonare <47446377+Resonare@users.noreply.github.com>
2019-12-18 20:34:33 +1100

    sad

--- assets/php/classes/user.php
+++ assets/php/classes/user.php
@@ -19,11 +19,11 @@ class User{
 
   function signIn(){
     echo "hi";
-  //   $login = htmlspecialchars($_POST["loginInFlags"]);
-  //   $pass = htmlspecialchars($_POST["passInFlags"]);
-  //   $_SESSION["loginInFlags"]=$login;
-  //   $_SESSION["passInFlags"]=$pass;
-  //
+    $login = htmlspecialchars($_POST["loginInFlags"]);
+    $pass = htmlspecialchars($_POST["passInFlags"]);
+    $_SESSION["loginInFlags"]=$login;
+    $_SESSION["passInFlags"]=$pass;
+  
   //   if (validatePassword($pass) && validateLogin($login)){
   //     $query = pg_query($connect, "SELECT id, password FROM ourusers WHERE login = '".$login."'");
   //     $numrows = pg_num_rows($query);
