aa18ea90dadbdd3958bc0a0612afbc9e16ee4db3
Resonare <47446377+Resonare@users.noreply.github.com>
2019-12-18 20:31:06 +1100

    sad

--- logPage.php
+++ logPage.php
@@ -2,9 +2,9 @@
   session_start();
   require "assets/php/functions/setConnection.php";
   $connect = setConnection();
-  // require "assets/php/classes/user.php";
+  require "assets/php/classes/user.php";
   // $user = new User;
-  // $user -> signIn();
+  // $user->signIn();
  ?>
 <html>
 <head>
