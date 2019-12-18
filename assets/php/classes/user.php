4eed6ae6058b4d06d2c53069de7f1a1baf6b2ed6
Resonare <47446377+Resonare@users.noreply.github.com>
2019-12-18 20:33:49 +1100

    sad

--- assets/php/classes/user.php
+++ assets/php/classes/user.php
@@ -18,28 +18,28 @@ class User{
   }
 
   function signIn(){
-    // echo "hi";
-    $login = htmlspecialchars($_POST["loginInFlags"]);
-    $pass = htmlspecialchars($_POST["passInFlags"]);
-    $_SESSION["loginInFlags"]=$login;
-    $_SESSION["passInFlags"]=$pass;
-
-    if (validatePassword($pass) && validateLogin($login)){
-      $query = pg_query($connect, "SELECT id, password FROM ourusers WHERE login = '".$login."'");
-      $numrows = pg_num_rows($query);
-      if($numrows == 0){
-        $error_loginInFlags = "User does not exist";
-      } else {
-        while($row = pg_fetch_row($query)){
-          if($row[1] == $pass){
-            setcookie("userIdInFlags", $row[0]);
-            header("Location: welcome.php");
-          } else {
-            $error_passInFlags = "Wrong password";
-          }
-        }
-      }
-    }
+    echo "hi";
+  //   $login = htmlspecialchars($_POST["loginInFlags"]);
+  //   $pass = htmlspecialchars($_POST["passInFlags"]);
+  //   $_SESSION["loginInFlags"]=$login;
+  //   $_SESSION["passInFlags"]=$pass;
+  //
+  //   if (validatePassword($pass) && validateLogin($login)){
+  //     $query = pg_query($connect, "SELECT id, password FROM ourusers WHERE login = '".$login."'");
+  //     $numrows = pg_num_rows($query);
+  //     if($numrows == 0){
+  //       $error_loginInFlags = "User does not exist";
+  //     } else {
+  //       while($row = pg_fetch_row($query)){
+  //         if($row[1] == $pass){
+  //           setcookie("userIdInFlags", $row[0]);
+  //           header("Location: welcome.php");
+  //         } else {
+  //           $error_passInFlags = "Wrong password";
+  //         }
+  //       }
+  //     }
+  //   }
   }
 }
  ?>
