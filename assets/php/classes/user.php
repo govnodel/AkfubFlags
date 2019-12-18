18f3964205e21b815e1c56c1f4f810d9390fbcdd
Resonare <47446377+Resonare@users.noreply.github.com>
2019-12-18 20:37:46 +1100

    sad

--- assets/php/classes/user.php
+++ assets/php/classes/user.php
@@ -5,14 +5,14 @@ class User{
 
   function validateLogin($l){
     if(strlen($l)==0){
-      $error_loginInFlags="Enter login";
+      $this->$error_loginInFlags="Enter login";
       return false;
     } else return true;
   }
 
   function validatePassword($p){
     if (strlen($p)==0){
-      $error_passInFlags="Enter password";
+      $this->$error_passInFlags="Enter password";
       return false;
     } else return true;
   }
@@ -24,21 +24,21 @@ class User{
     $_SESSION["loginInFlags"]=$login;
     $_SESSION["passInFlags"]=$pass;
 
-    if (validatePassword($pass) && validateLogin($login)){
-  //     $query = pg_query($connect, "SELECT id, password FROM ourusers WHERE login = '".$login."'");
-  //     $numrows = pg_num_rows($query);
-  //     if($numrows == 0){
-  //       $error_loginInFlags = "User does not exist";
-  //     } else {
-  //       while($row = pg_fetch_row($query)){
-  //         if($row[1] == $pass){
-  //           setcookie("userIdInFlags", $row[0]);
-  //           header("Location: welcome.php");
-  //         } else {
-  //           $error_passInFlags = "Wrong password";
-  //         }
-  //       }
-  //     }
+    if ($this->validatePassword($pass) && $this->validateLogin($login)){
+      $query = pg_query($connect, "SELECT id, password FROM ourusers WHERE login = '".$login."'");
+      $numrows = pg_num_rows($query);
+      if($numrows == 0){
+        $this->$error_loginInFlags = "User does not exist";
+      } else {
+        while($row = pg_fetch_row($query)){
+          if($row[1] == $pass){
+            setcookie("userIdInFlags", $row[0]);
+            header("Location: welcome.php");
+          } else {
+            $this->$error_passInFlags = "Wrong password";
+          }
+        }
+      }
     }
   }
 }
