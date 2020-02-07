2fb2de491a6a692c4ff5b347f936de5d2e676aad
Resonare <47446377+Resonare@users.noreply.github.com>
2020-02-07 17:53:41 +1100

    sad

--- gayme.php
+++ gayme.php
@@ -51,13 +51,13 @@
 <script type="text/javascript">
   var names = [];
   var pops = [];
-  // var caps = [];
+  var capitals = [];
 
   <?php
   while ($row = pg_fetch_row($result)) {
     echo "names[names.length] =  '$row[0]';";
     echo "pops[pops.length] =  '$row[1]';";
-    // echo "caps[caps.length] =  '$row[2]';";
+    echo "capitals[capitals.length] =  '$row[2]';";
   }
   ?>
 </script>
