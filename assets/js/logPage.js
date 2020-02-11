c5b1a35f898f1e03b921318747925ea3409981c7
Resonare <47446377+Resonare@users.noreply.github.com>
2020-02-11 22:56:53 +1100

    sad

--- assets/js/logPage.js
+++ assets/js/logPage.js
@@ -1,10 +1,11 @@
 $(function() {
-  if (document.cookie.replace(/(?:(?:^|.*;\s*)enterAttempInFlags\s*\=\s*([^;]*).*$)|^.*$/, "$1") == 1) {
+  if (document.cookie.replace(/(?:(?:^|.*;\s*)enterAttempInFlags\s*\=\s*([^;]*).*$)|^.*$/, "$1") != 1) {
     $('div').animate({
   		marginTop: '12vh',
       opacity: 1
   	}, 700);
   } else {
+    document.cookie = "enterAttempInFlags=;";
     $('div').animate({
   		marginTop: '12vh',
       opacity: 1
