15e7e80642cca95eae9b0aa17dcf92f587cb4bc4
Resonare <47446377+Resonare@users.noreply.github.com>
2020-02-11 23:40:22 +1100

    sad

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -3,7 +3,11 @@ $(function() {
     opacity: 1
   }, 500);
 
-  // setTimeout(function(){
-  //   window.location.href = "http://flags.alfa-omega.pro";
-  // }, 3000);
+  setTimeout(function(){
+    $('#text').animate({
+      opacity: 0
+    }, 400, function(){
+      window.location.href = "http://flags.alfa-omega.pro";
+    });
+  }, 3000);
 });
