ae6a8f8494c71b3c057a1bcc23d02b0375e876b8
Resonare <47446377+Resonare@users.noreply.github.com>
2020-02-12 19:42:56 +1100

    sad

--- assets/php/classes/user.php
+++ assets/php/classes/user.php
@@ -65,7 +65,7 @@ class User{
       if($numrows == 0){
         $hash = password_hash($pass, PASSWORD_DEFAULT);
 
-        $result = pg_query($this->connect, "INSERT INTO ourusers(login, password, mail) VALUES ('".$login."', '".$hash."', '".$email."');");
+        $result = pg_query($this->connect, "INSERT INTO ourusers(login, password, mail) VALUES ('$login', '$hash', '$email');");
         $query = pg_query($this->connect, "SELECT id FROM ourusers WHERE login = '".$login."';");
 
         while($row = pg_fetch_row($query)){
