38e836e6dafcc51dc882c560761a35a731412bb0
Resonare <47446377+Resonare@users.noreply.github.com>
2020-03-04 12:10:58 +1100

    sad

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,14 +1,14 @@
 $(function() {
-  $('#text').animate({
+  $('.text').animate({
     opacity: 1
   }, 500);
 
   setTimeout(function(){
     //document.body.style.animation = "end .4s linear forwards";
-    $('#text').animate({
-      // opacity: 0
+    $('.text').animate({
+      opacity: 0
     }, 300, function(){
-      // window.location.href = "http://flags.alfa-omega.pro";
+      window.location.href = "http://flags.alfa-omega.pro";
     });
   }, 1500);
 });
