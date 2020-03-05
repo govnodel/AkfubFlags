0f0285de7ee0d8155900c564f31ba012f0bae728
Resonare <47446377+Resonare@users.noreply.github.com>
2020-03-05 19:13:47 +1100

    sad

--- assets/js/winornotpage.js
+++ assets/js/winornotpage.js
@@ -1,10 +1,10 @@
 var WIDTH = document.documentElement.clientWidth;
-var start = (WIDTH - 10 * document.getElementById("firstFlags").children.length) / 2;
+var start = (WIDTH - 10 * document.getElementById("firstFlags").children.length) / 2 / WIDTH * 100;
 
 document.body.style.animation = "end 0.8s linear forwards";
 
 for (var i = 0; i < document.getElementById("firstFlags").children.length; i++) {
-  document.getElementById("firstFlags").children[i].style.left = 10 * i + (start / WIDTH * 100) + "%";
+  document.getElementById("firstFlags").children[i].style.left = 10 * i + start + "%";
 }
 
 for (var i = 0; i < document.getElementById("secondFlags").children.length; i++) {
