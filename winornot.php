4b00b3bfacb558ee230f33b179f87b1aa969c998
Resonare <47446377+Resonare@users.noreply.github.com>
2020-03-10 18:50:59 +1100

    sad

--- winornot.php
+++ winornot.php
@@ -11,7 +11,7 @@
     echo
     "<div id='nameContainer'>
       <canvas id='canvas' width='200' height='200'></canvas>
-      <p>".$_COOKIE["nameOldInFlag"]."</p>
+      <p id='name'>".$_COOKIE["nameOldInFlag"]."</p>
     </div>
     <div id='tableContainer'>
       <table id='table'>
