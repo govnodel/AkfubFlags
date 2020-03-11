07f8a3751587b5b8b797e682803e2139bc0ecfbe
Resonare <47446377+Resonare@users.noreply.github.com>
2020-03-11 13:06:44 +1100

    sad

--- index.php
+++ index.php
@@ -155,6 +155,7 @@
     var avatar = "<?=$avatar?>";
   </script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src='assets/js/flagConstructor.js'></script>
   <script src="assets/js/mainScripts.js"></script>
 </body>
 </html>
