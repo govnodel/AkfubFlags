b1f1a9e206961fc55a21aed5f33927e56b9b779b
Resonare <47446377+Resonare@users.noreply.github.com>
2020-03-11 13:11:48 +1100

    sad

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
       <div id="logRegMenu">
         <?php
           if ($authorized) {
-            echo "<canvas id='canvas' width='150' height='150'></canvas>";
+            echo "<canvas id='canvas' width='160' height='160'></canvas>";
             echo "<p>".$name."</p>";
           } else {
             echo "<h1 id = 'login'>Log in</h1>";
