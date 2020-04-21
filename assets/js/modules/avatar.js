8e9602c0ce69d156c2112aa7e38946d73a28586f
Resonare <47446377+Resonare@users.noreply.github.com>
2020-04-21 15:05:25 +1100

    sad

--- assets/js/modules/avatar.js
+++ assets/js/modules/avatar.js
@@ -82,7 +82,7 @@ function draw(str, cnv){
       ctx.stroke();
       break;
     default:
-      let proc = 40;
+      let proc = 55;
 
       ctx.fillStyle = 'white';
       ctx.fillRect(0, 0, size, size);
